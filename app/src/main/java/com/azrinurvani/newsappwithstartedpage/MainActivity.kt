33ed1ead419a5b480bba25fe6ea0c6c84e475040
azrinurvani <azrinurvani@gmail.com>
2024-10-10 15:47:55 +0700

    1. Implementation Navigation Graph 2. Set condition Navigate to OnBoardingScreen or NewsScreen 3. Implementation systemUiController to change System Bar Colors

--- app/src/main/java/com/azrinurvani/newsappwithstartedpage/MainActivity.kt
+++ app/src/main/java/com/azrinurvani/newsappwithstartedpage/MainActivity.kt
@@ -1,58 +1,59 @@
 package com.azrinurvani.newsappwithstartedpage
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.azrinurvani.newsappwithstartedpage.domain.usecases.AppEntryUseCases
-import com.azrinurvani.newsappwithstartedpage.presentation.onboarding.OnBoardingScreen
-import com.azrinurvani.newsappwithstartedpage.presentation.onboarding.OnBoardingViewModel
+import com.azrinurvani.newsappwithstartedpage.presentation.nav_graph.NavGraph
 import com.azrinurvani.newsappwithstartedpage.ui.theme.NewsAppWithStartedPageTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var useCases: AppEntryUseCases
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         WindowCompat.setDecorFitsSystemWindows(window,false)
-        installSplashScreen()
-
-        lifecycleScope.launch {
-            useCases.readAppEntry().collect{
-                Log.d("MainActivity", "test readAppEntry: $it")
+        installSplashScreen().apply {
+            //keep splash screen still visible until fetch the start destination
+            //from data store preferences
+            setKeepOnScreenCondition{
+                viewModel.splashCondition
             }
         }
 
         setContent {
             NewsAppWithStartedPageTheme {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 Box(
                     modifier = Modifier.background(MaterialTheme.colorScheme.background)
                 ) {
-                    val viewModel : OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(
-                        event = viewModel::onEvent
+                    val startDestination = viewModel.startDestination
+                    NavGraph(
+                        startDestination = startDestination
                     )
                 }
             }
